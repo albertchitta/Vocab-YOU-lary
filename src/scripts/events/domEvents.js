b7488f1fa305c739b21e9e9e92065350bbcbc4c5
albertchitta <albert.w.chittaphong@gmail.com>
2021-09-09 20:52:27 -0500

    Completed update and delete

--- src/scripts/events/domEvents.js
+++ src/scripts/events/domEvents.js
@@ -1,10 +1,16 @@
+import addVocabForm from '../components/forms/addVocabForm';
 import { showVocab } from '../components/vocab';
-import { createVocab } from '../helpers/data/vocabData';
+import {
+  createVocab,
+  getSingleVocab,
+  updateVocab,
+  deleteVocab
+} from '../helpers/data/vocabData';
 
 const domEvents = (uid) => {
   document.querySelector('#main-container').addEventListener('click', (e) => {
     // CLICK EVENT FOR SUBMITTING A FORM TO ADD A VOCAB
-    if (e.target.id.includes('submit-vocab')) {
+    if (e.target.id === ('submit-vocab')) {
       e.preventDefault();
       const vocabObject = {
         title: document.querySelector('#title').value,
@@ -14,6 +20,34 @@ const domEvents = (uid) => {
       };
 
       createVocab(vocabObject).then(showVocab);
+    }
+
+    // CLICK EVENT FOR EDITING/UPDATING A VOCAB
+    if (e.target.id.includes('edit')) {
+      const [, firebaseKey] = e.target.id.split('--');
+      getSingleVocab(firebaseKey).then((vocabObject) => addVocabForm(uid, vocabObject));
+    }
+    if (e.target.id.includes('update-vocab')) {
+      e.preventDefault();
+      const [, firebaseKey] = e.target.id.split('--');
+      const vocabObject = {
+        title: document.querySelector('#title').value,
+        definition: document.querySelector('#definition').value,
+        category_id: document.querySelector('#category_id').value,
+        firebaseKey,
+        uid
+      };
+
+      updateVocab(vocabObject).then(showVocab);
+    }
+
+    // CLICK EVENT FOR DELETING A VOCAB
+    if (e.target.id.includes('delete')) {
+      // eslint-disable-next-line no-alert
+      if (window.confirm('Want to delete?')) {
+        const [, firebaseKey] = e.target.id.split('--');
+        deleteVocab(uid, firebaseKey).then(showVocab);
+      }
     }
   });
 };
